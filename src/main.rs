0ddfe5ea3902d9b425346b8edd6192a22a53a1cc
AqourSoro <971755995@qq.com>
2023-12-28 18:25:04 +0800

    Update log4rs.yaml
    Update yaml to enable different level of logs for cmd and files.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use log4rs;
 fn main() 
 {
     log4rs::init_file("./config/log4rs.yaml", Default::default()).unwrap();
+    trace!("detailed tracing info for debug in Nexus Music!");
     info!("The main program of Nexus Music in Rust!");
     debug!("use the functions from graphics and sounds: {}", crate_usage(graphics_nexus::add_in_graphics, sound_nexus::add_in_sound));
 }
