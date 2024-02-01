09ed2eb2d41489013433ada175292c2933e3320a
AqourSoro <971755995@qq.com>
2024-02-02 01:41:03 +0800

    Update event_handler.rs

--- noa/src/event/event_handler.rs
+++ noa/src/event/event_handler.rs
@@ -1,12 +1,49 @@
+use std::{cell::RefCell, rc::Weak, sync::Arc};
+
+use log::debug;
+
 use super::event::Event;
 
-pub trait Listener
+
+pub trait Invokable
 {
-    fn notify(&mut self, event:Event);
+    fn Invoke(&mut self, event:Event);
 }
 
-pub trait Dispatchable<F>
+pub trait Dispatchable
 {
-    fn register(&mut self, callbacks: F) 
-        where F: Fn(&Event) + 'static;
+    fn add_listener(&mut self, listener:Arc<RefCell<(Event, impl Fn(&Event) + 'static)>>); 
+}
+
+pub struct DefaultListener
+{
+    listeners: Vec<Weak<RefCell<(Event, dyn Fn(&Event) + 'static)>>> 
+}
+
+impl Invokable for DefaultListener
+{
+    fn Invoke(&mut self, event:Event) 
+    {
+        let ref_listener = self.listeners.clone();
+        let callbacks = ref_listener.into_iter().filter(|listener|
+        {
+            if let Some(weak_listener) = listener.upgrade()
+            {
+                if let Ok(listener) = weak_listener.try_borrow()
+                {
+                    return event == listener.0;
+                }
+                else
+                {
+                    debug!("Failed to borrow the RefCell");
+                    return false;
+                }
+            }
+            else 
+            {
+                debug!("Weak reference is no longer valid");
+                return false;
+            }
+        });
+    }
 }
